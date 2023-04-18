98cda2769ad3594434850f72dbfef67129c210de
ricardo-werner <wernerdive@gmail.com>
2023-04-18 11:38:49 -0300

    refactor: Change Register.css

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/auth";
+import "./Register.css";
 
 const RegisterPage = () => {
     //recuperar o callback do logout do contexto
@@ -12,14 +13,19 @@ const RegisterPage = () => {
     };
 
     return (
-        <>
-            <h1>Registro de Farmácias e Medicamentos</h1>
-            <Link to="/addstore"><button>Registro de Farmácias</button></Link>
-            <Link to="/addmedication"><button>Registro de Medicamentos</button></Link>
+        <div className="container">
+            <div className="link-container">
+                <h1>Registro de Farmácias e Medicamentos</h1>
+                <nav className="nav-link">
+                <Link to="/addstore"><button>Registro de Farmácias</button></Link>
+                <Link to="/addmedication"><button>Registro de Medicamentos</button></Link>
+                </nav>
+            </div>
+        
 
             {/*<p>{String(authenticated)}</p>*/}
             <button className="btn-logout" onClick={handleLogout}>Logout</button>
-        </>
+        </div>
     );
 }
 
