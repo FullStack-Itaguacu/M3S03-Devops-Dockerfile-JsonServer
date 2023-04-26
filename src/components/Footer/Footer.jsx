8bde75e1491f79e669098d28e062204c9243720c
ricardo-werner <wernerdive@gmail.com>
2023-04-26 14:55:34 -0300

    refactor: Footer .jsx e .css fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,8 +20,10 @@ const Footer = () => {
   return (
     <footer className="footer-container">
       <div>
-          <h6>&copy; Copyright Pharmacy Central System - {new Date().getFullYear()}</h6>
-          <h6>Desenvolvido por Ricardo Werner {new Date().getFullYear()}</h6>
+          <h6 className='copy'>&copy; Copyright Pharmacy Central System - {new Date().getFullYear()}</h6>
+      </div>
+      <div>    
+          <h6 className='dev'>Desenvolvido por Ricardo Werner - LAB365-FullStack-Itaguaçu {new Date().getFullYear()}</h6>
       </div>
     </footer>
   );
